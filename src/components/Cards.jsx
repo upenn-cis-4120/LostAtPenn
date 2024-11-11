38ea261651ef4b3854e953c9cabc7c501c06643b
Jimin Choi <72880905+saordoe@users.noreply.github.com>
2024-11-10 22:09:50 -0500

    lost and found cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const Cards = () => {
+const Cards = ({status, item, when, where, photo}) => {
   return (
-    <div className="bg-gray-100 w-20 h-20 rounded-lg">
-        <h1>
-            Cards
+    <div className="bg-gray-100 w-1/5 h-3/5 rounded-3xl">
+      <div className="h-1/7 w-full bg-cblue rounded-t-3xl flex items-center">
+        <h1 className="text-left px-5 text-white font-bold text-xl">
+          {status}
         </h1>
-        <p>
-            Description
-        </p>
+      </div>
+      <p className="text-left p-5 text-l">
+        <strong>Item:</strong> {item} <br />
+        <strong>When:</strong> {when} <br />
+        <strong>Where:</strong> {where} <br />
+      </p>
+      <img src={photo} alt="item" className="px-6 w-full h-1/2 rounded-xl" />
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Cards from './Cards'
+import macbook from '../assets/mbpro.jpg'
+
 
 const Hero = () => {
   return (
     <div className="bg-red-100 w-screen h-screen">
-        <Cards />
+        <Cards status="Found" item="Macbook Pro 13'" when="April 13 2020" where="David Rittenhouse" photo={macbook}/>
     </div>
   )
 }
