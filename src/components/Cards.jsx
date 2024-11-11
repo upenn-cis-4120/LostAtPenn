d7ad5a26e0d1fecf4a7758df3cf3273765df0566
Jimin Choi <72880905+saordoe@users.noreply.github.com>
2024-11-10 23:00:39 -0500

    Cards updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 
-const Cards = ({status, item, when, where, photo}) => {
+const Cards = ({ status, item, when, where, photo }) => {
   return (
-    <div className="bg-gray-100 w-1/5 h-3/5 rounded-3xl">
-      <div className="h-1/7 w-full bg-cblue rounded-t-3xl flex items-center">
+    <div className={`bg-gray-100 w-[300px] ${photo ? 'h-[calc(60vh-1rem)]' : 'h-[calc(30vh-1rem)]'} rounded-3xl flex flex-col overflow-hidden shadow-lg`}>
+      <div className={`h-28 w-full ${status === 'Lost' ? 'bg-red-500' : 'bg-cblue'} rounded-t-3xl flex items-center justify-center`}>
         <h1 className="text-left px-5 text-white font-bold text-xl">
           {status}
         </h1>
       </div>
+
       <p className="text-left p-5 text-l">
         <strong>Item:</strong> {item} <br />
         <strong>When:</strong> {when} <br />
         <strong>Where:</strong> {where} <br />
       </p>
-      <img src={photo} alt="item" className="px-6 w-full h-1/2 rounded-xl" />
+
+      {photo && (
+        <div className="px-6 w-full h-1/2 rounded-xl overflow-hidden">
+          <img className="w-full h-full object-cover" src={photo} alt="item" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Cards;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,59 @@
-import React from 'react'
-import Cards from './Cards'
-import macbook from '../assets/mbpro.jpg'
-
+import React from 'react';
+import Cards from './Cards';
+import macbook from '../assets/mbpro.jpg';
 
 const Hero = () => {
   return (
-    <div className="bg-red-100 w-screen h-screen">
-        <Cards status="Found" item="Macbook Pro 13'" when="April 13 2020" where="David Rittenhouse" photo={macbook}/>
+    <div className="bg-white w-screen h-screen overflow-y-auto p-4 flex flex-wrap gap-16 justify-center">
+      <Cards 
+        status="Found" 
+        item="Macbook Pro 13'" 
+        when="April 13 2020" 
+        where="David Rittenhouse" 
+        photo={macbook}
+      />
+      <Cards 
+        status="Found" 
+        item="Macbook Pro 13'" 
+        when="April 13 2020" 
+        where="David Rittenhouse" 
+        photo={macbook}
+      />
+      <Cards 
+        status="Found" 
+        item="Macbook Pro 13'" 
+        when="April 13 2020" 
+        where="David Rittenhouse" 
+        photo={macbook}
+      />
+      <Cards 
+        status="Found" 
+        item="Macbook Pro 13'" 
+        when="April 13 2020" 
+        where="David Rittenhouse" 
+        photo={macbook}
+      />
+      <Cards 
+        status="Found" 
+        item="Macbook Pro 13'" 
+        when="April 13 2020" 
+        where="David Rittenhouse"
+      />
+      <Cards 
+        status="Found" 
+        item="Macbook Pro 13'" 
+        when="April 13 2020" 
+        where="David Rittenhouse" 
+        photo={macbook}
+      />
+      <Cards
+        status="Lost"
+        item=""
+        when="April 13 2020"
+        where="David Rittenhouse"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
