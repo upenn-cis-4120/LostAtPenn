2e7618787d5f43b9c02b817507163d680059dfc9
Ximing Luo <ximing.luo@kyanon.digital>
2024-11-11 00:21:46 -0500

    Chat Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Chat from './components/Chat' // Import Chat component
 
 const App = () => {
   return (
-    <div className="flex flex-col">
-      <Navbar />
-      <Hero />
-    </div>
+    <Router>
+      <div className="flex flex-col">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} /> {/* Default route for Hero */}
+          <Route path="/chat" element={<Chat />} /> {/* Route for Chat component */}
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
