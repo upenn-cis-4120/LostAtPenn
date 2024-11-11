2b5f72fffd7bf5bccdb13443766aaba7df833175
Jimin Choi <72880905+saordoe@users.noreply.github.com>
2024-11-10 23:05:12 -0500

    Cards updated (visual changes)

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 const Cards = ({ status, item, when, where, photo }) => {
   return (
-    <div className={`bg-gray-100 w-[300px] ${photo ? 'h-[calc(60vh-1rem)]' : 'h-[calc(30vh-1rem)]'} rounded-3xl flex flex-col overflow-hidden shadow-lg`}>
+    <div className={`bg-gray-100 w-[300px] ${photo ? 'h-[calc(60vh-1rem)]' : 'h-[calc(30vh-1rem)]'} rounded-3xl flex flex-col overflow-hidden shadow-lg`}
+         style={{ boxShadow: '4px 0 15px rgba(0, 0, 0, 0.1), -4px 0 15px rgba(0, 0, 0, 0.1), 0 4px 15px rgba(0, 0, 0, 0.1)' }}>
       <div className={`h-28 w-full ${status === 'Lost' ? 'bg-red-500' : 'bg-cblue'} rounded-t-3xl flex items-center justify-center`}>
-        <h1 className="text-left px-5 text-white font-bold text-xl">
+        <h1 className="text-left px-5 text-white font-bold text-4xl">
           {status}
         </h1>
       </div>
 
-      <p className="text-left p-5 text-l">
-        <strong>Item:</strong> {item} <br />
-        <strong>When:</strong> {when} <br />
-        <strong>Where:</strong> {where} <br />
-      </p>
+      <div className="text-left px-5 py-8 text-2xl space-y-2">
+        <p><strong>Item:</strong> {item}</p>
+        <p><strong>When:</strong> {when}</p>
+        <p><strong>Where:</strong> {where}</p>
+      </div>
 
       {photo && (
         <div className="px-6 w-full h-1/2 rounded-xl overflow-hidden">
